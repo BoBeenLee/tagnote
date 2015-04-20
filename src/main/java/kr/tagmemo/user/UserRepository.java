b081e45924fc29c0739a1298226802701097fee0
BoBinLee <cultist_tp@naver.com>
2015-04-20 23:43:37 +0900

    update package name

--- src/main/java/kr/tagmemo/user/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.tagmemo.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository  extends JpaRepository<User, Long>{
-	public User findByEmail(String email) ;
-}
